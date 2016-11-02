a86a6a68ed52d022b19d85109c8f5c7e50df03a2
Balazs <blukucz@gmail.com>
2016-11-02 09:40:34 +0100

    Withdrawal Simulator v0.4

--- src/Client/SQLab/wwwroot/js/lbMathTools.js
+++ src/Client/SQLab/wwwroot/js/lbMathTools.js
@@ -1,5 +1,5 @@
-﻿// returns a gaussian random function with the given mean and stdev.
-function gaussian(mean, stdev) {
+﻿function lbgaussian(mean, stdev) {
+    // returns a gaussian random function with the given mean and stdev.
     var y2;
     var use_last = false;
     return function () {
@@ -27,11 +27,29 @@ function gaussian(mean, stdev) {
     }
 }
 
-function average(arrayB) {
+function lbaverage(arrayB) {
     var total = 0;
     for (var i = 0; i < arrayB.length; i++) {
         total += arrayB[i];
     }
     var avg = total / arrayB.length;
     return avg;
+}
+
+function lbmedian(arrayB) {
+    arrayB.sort(function (a, b) {
+        return a - b;
+    });
+    var i = arrayB.length / 2;
+    i % 1 === 0 ? med = (arrayB[Math.floor(i) - 1] + arrayB[Math.floor(i)]) / 2 : med = arrayB[Math.floor(i)];
+    return med;
+}
+function lbstdDev(arrayB) {
+    var avg = lbaverage(arrayB);
+    var sumdev = 0;
+    for (var i = 0; i < arrayB.length; i++) {
+        sumdev += (arrayB[i]-avg)*(arrayB[i]-avg);
+    }
+    var stdDev = Math.sqrt(sumdev/(arrayB.length-1));
+    return stdDev;
 }
