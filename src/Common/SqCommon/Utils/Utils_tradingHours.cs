8d0fe0ba9858ef2fc22fe34329906911df38a1f6
gyantal <gyantal@users.noreply.github.com>
2023-12-30 23:48:25 +0000

    fixing the new HTML format of https://www.nyse.com/markets/hours-calendars

--- src/Common/SqCommon/Utils/Utils_tradingHours.cs
+++ src/Common/SqCommon/Utils/Utils_tradingHours.cs
@@ -72,7 +72,7 @@ namespace SqCommon
                 int year1 = -1, year2 = -1;
                 var trs = holidayTable.Split(new string[] { "<tr>\n  ", "<tr>", "<tr style=", "</tr>\n  ", "</tr>" }, StringSplitOptions.RemoveEmptyEntries);
                 var headerRow = trs[1];
-                var tdsHeader = headerRow.Split(new string[] { @"<th>", @"</th>" }, StringSplitOptions.RemoveEmptyEntries);
+                var tdsHeader = headerRow.Split(new string[] { @"<td>", @"</td>" }, StringSplitOptions.RemoveEmptyEntries); // 2023-12-28: a "<th>" => "<td>"
                 year1 = Int32.Parse(tdsHeader[1]);
                 year2 = Int32.Parse(tdsHeader[2]);
                 //year3 = Int32.Parse(tdsHeader[7]);  // there is year3 too, but we don't need it in VBroker or healthmonitor. So, just ignore them
